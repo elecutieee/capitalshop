7fe6b7bba9ae5709a26d048811465b9f92f7e812
Eleser <gutzele29@gmail.com>
2024-06-15 20:00:08 +0800

    Add files via upload

---
+++ quizgame/quizgame/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace quizgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblquestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::quizgame.Properties.Resources.quizzz;
+            this.pictureBox1.Location = new System.Drawing.Point(170, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(677, 224);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblquestion
+            // 
+            this.lblquestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblquestion.Location = new System.Drawing.Point(58, 251);
+            this.lblquestion.Name = "lblquestion";
+            this.lblquestion.Size = new System.Drawing.Size(841, 134);
+            this.lblquestion.TabIndex = 1;
+            this.lblquestion.Text = "Question";
+            this.lblquestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Peru;
+            this.button1.Location = new System.Drawing.Point(170, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Peru;
+            this.button3.Location = new System.Drawing.Point(170, 531);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 56);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Peru;
+            this.button4.Location = new System.Drawing.Point(547, 520);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(300, 56);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Peru;
+            this.button2.Location = new System.Drawing.Point(547, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::quizgame.Properties.Resources.historyy;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(945, 682);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblquestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "QUIZ GAME";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblquestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ quizgame/quizgame/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace quizgame
+{
+    public partial class Form1 : Form
+    { //quizgame variables
+        int correctanswer;
+        int questionNumber;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 10;
+
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {       
+
+            var senderobject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderobject.Tag);
+
+            if (buttonTag == correctanswer)
+            {
+                score++;
+
+            }
+            if (questionNumber == totalQuestions)
+            {
+                //work out the percentage
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+
+                    "QUIZ ENDED" + Environment.NewLine +
+                    "YOU HAVE ANSWERED  " + score + "questions correctly  " + Environment.NewLine +
+                    "YOUR TOTAL PERCENTAGE IS " + percentage + "%" + Environment.NewLine +
+                    "Click OK to play again"
+                    );
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+            }
+            questionNumber++;
+            askQuestion(questionNumber);
+
+
+
+
+
+
+        }
+        private void askQuestion(int qnum)
+        {
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources._1;
+                    lblquestion.Text = "Which of the following best signifies this image?";
+                    button1.Text = "war at zweibrucken,german";
+                    button2.Text = "people celebrating christmas";
+                    button3.Text = "planting of tree of liberty";
+                    button4.Text = "none of the above";
+
+                    correctanswer = 3;
+
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources._2;
+                    lblquestion.Text = "the painting 'the dream of worldwide democratic and social republics' was prepared by whom";
+                    button1.Text = "Giuseppe Mazzini";
+                    button2.Text = "Frederic Sorrieu";
+                    button3.Text = "Henry Patulfo";
+                    button4.Text = "Duke Metternich";
+
+                    correctanswer = 2;
+
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._3;
+                    lblquestion.Text = "Which of the following aspect best signifies this image?";
+                    button1.Text = "Round table conference at london";
+                    button2.Text = "Consistuent assembly of india";
+                    button3.Text = "the frankfurt parliament in the church of st.paul";
+                    button4.Text = "the hall of mirrors of versailles";
+
+                    correctanswer = 3;
+
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources._4;
+                    lblquestion.Text = "what is shown in this picture as the basis of britain?";
+                    button1.Text = "the sea routes which britain controlled";
+                    button2.Text = "brittania sitting on the globe";
+                    button3.Text = "its various colonies";
+                    button4.Text = "its domination of the world";
+
+                    correctanswer = 2;
+
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5;
+                    lblquestion.Text = "Which of the following best describe his images by julius hubner?";
+                    button1.Text = "defeat of otto von bismarck";
+                    button2.Text = "the fallen germania";
+                    button3.Text = "sleeping lady";
+                    button4.Text = "none of the above";
+
+                    correctanswer = 2;
+
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._6;
+                    lblquestion.Text = "Which of the following event was related to this image of 'gandhiji'";
+                    button1.Text = "Non- cooperation movement";
+                    button2.Text = "kheda satyagraha";
+                    button3.Text = "dandi march";
+                    button4.Text = "none of the above";
+
+                    correctanswer = 3;
+
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources._7;
+                    lblquestion.Text = "which following aspect best signifies this image of germania?";
+                    button1.Text = "heroism and justice";
+                    button2.Text = "folk and clutural tradition";
+                    button3.Text = "austerity and asceticism";
+                    button4.Text = "revenge and vengeance";
+
+                    correctanswer = 1;
+                    break;
+
+                case 8:
+                    pictureBox1.Image = Properties.Resources._8;
+                    lblquestion.Text = "the event in the image is the explains the reason for the flee of louis philippine in 1948?";
+                    button1.Text = "peasant uprising";
+                    button2.Text = "revolution of liberals";
+                    button3.Text = "outbreak of freanch revolution";
+                    button4.Text = "war between france and prussia";
+
+                    correctanswer = 1;
+
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources._9;
+                    lblquestion.Text = "who is shown in this picture sitting on knees?";
+                    button1.Text = "giuseppe mazzani";
+                    button2.Text = "guiseppe garbaldi";
+                    button3.Text = "victor emmanuel 2nd";
+                    button4.Text = "cavour";
+
+                    correctanswer = 2;
+
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources._10;
+                    lblquestion.Text = "how is bharat mata portrayed in the above picture";
+                    button1.Text = "ascetic";
+                    button2.Text = "divine";
+                    button3.Text = "spiritual";
+                    button4.Text = "all of the above";
+
+                    correctanswer = 4;
+
+                    break;
+
+
+
+
+            }
+
+        }
+
+        }
+    }
+
+
+
